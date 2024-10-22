3a65bb8919e74e72726f9be34d1b2e5342d7a5c1
Berkay Yelken <berkayelken@gmail.com>
2024-10-23 00:41:10 +0300

    map config was added

--- src/main/java/io/github/oasisframework/hazelcast/three/helper/connection/HazelcastConnection.java
+++ src/main/java/io/github/oasisframework/hazelcast/three/helper/connection/HazelcastConnection.java
@@ -3,12 +3,19 @@ package io.github.oasisframework.hazelcast.three.helper.connection;
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.client.config.ClientNetworkConfig;
+import com.hazelcast.config.Config;
+import com.hazelcast.config.MapConfig;
 import com.hazelcast.config.SerializerConfig;
 import com.hazelcast.core.HazelcastInstance;
-import io.github.oasisframework.hazelcast.common.properties.HazelcastConnectionAbstractProperties;
+import io.github.oasisframework.hazelcast.common.properties.HazelcastConnectionProperties;
+import io.github.oasisframework.hazelcast.common.properties.HazelcastMapProperty;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Objects;
 
 @Configuration
 public class HazelcastConnection {
@@ -19,17 +26,27 @@ public class HazelcastConnection {
     private static final String INVOCATION_TIMEOUT_KEY = "hazelcast.client.invocation.timeout.seconds";
     private static final String INVOCATION_TIMEOUT_VALUE = "1";
 
-    private final HazelcastConnectionAbstractProperties hazelcastConnectionProperties;
+    private final HazelcastConnectionProperties hazelcastConnectionProperties;
     private final SerializerConfig customConfig;
 
-    public HazelcastConnection(HazelcastConnectionAbstractProperties hazelcastConnectionProperties, SerializerConfig customConfig) {
+    public HazelcastConnection(HazelcastConnectionProperties hazelcastConnectionProperties, SerializerConfig customConfig) {
         this.hazelcastConnectionProperties = hazelcastConnectionProperties;
         this.customConfig = customConfig;
     }
 
+    @Bean
+    @ConditionalOnMissingBean (SerializerConfig.class)
+    public SerializerConfig dummySerializerConfig() {
+        return new SerializerConfig();
+    }
+
     @Bean(HAZELCAST_INSTANCE)
-    public HazelcastInstance hazelcastClient() {
-		return HazelcastClient.newHazelcastClient(createClientConfig());
+    public HazelcastInstance hazelCastClient() {
+        HazelcastInstance instance = HazelcastClient.newHazelcastClient(createClientConfig());
+
+        addMapsConfigs(instance.getConfig());
+
+        return instance;
     }
 
     private ClientConfig createClientConfig() {
@@ -50,6 +67,37 @@ public class HazelcastConnection {
         config.getGroupConfig().setPassword(hazelcastConnectionProperties.getConnectionPassword());
 
         return config;
+    }
+
+    private void addMapsConfigs(Config config) {
+        if (CollectionUtils.isEmpty(hazelcastConnectionProperties.getCreatableMaps())) {
+            return;
+        }
+
+        hazelcastConnectionProperties.getCreatableMaps().stream().filter(Objects::nonNull).map(this::createMapConfig).filter(Objects::nonNull)
+                .forEach(mapConfig -> addMapConfig(config, mapConfig));
+    }
+
+    private MapConfig createMapConfig(HazelcastMapProperty property) {
+        try {
+            MapConfig mapConfig = new MapConfig(property.getName());
+
+            mapConfig.setAsyncBackupCount(property.getMaxAsyncBackupCounts());
+            mapConfig.setMaxIdleSeconds(property.getMaxIdleSeconds());
+            mapConfig.setTimeToLiveSeconds(property.getTimeToLiveSeconds());
+
+            return mapConfig;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    private void addMapConfig(Config config, MapConfig mapConfig) {
+        try {
+            config.addMapConfig(mapConfig);
+        } catch (Exception ignored) {
+
+        }
     }
 
     private ClientNetworkConfig createClientNetworkConfig() {
